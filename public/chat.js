f0391970a4f4bfe87c1e01e6e3657503130a1e58
Galia Rabbani <galia.giti@gmail.com>
2020-03-06 23:47:26 +0600

    chatOutpu styling done

--- public/chat.js
+++ public/chat.js
@@ -11,14 +11,48 @@ sendButton.addEventListener('click', () => {
             chatboxMessage: clientChatInput.innerHTML
         }
     );
-    clientChatInput.innerHTML = ''
+     clientChatInput.innerHTML = ''
+     var name = clientName.value.split(" ");
+     
 })
 
 clientSocket.on('serverEmittingSomeonesWords', (roFromServer) => {
-    console.log('roFromServer: ')
-    console.log(roFromServer)
+    pushChatPortionToChatOutput(roFromServer)
 
-    var clientMessage = `<span class="clientMessage"> ${roFromServer.chatboxMessage} </span>`;
-    var clientName = roFromServer.clientName;
-    chatOutput.innerHTML += '<br/>' + clientName+ ': ' + clientMessage;
+
 })
+
+function pushChatPortionToChatOutput(ro){
+    var firstLastCharecterDiv = document.createElement("div")
+    var usernameDiv = document.createElement("div")
+    var messageDiv = document.createElement("div")
+    var chatPortionContainer = document.createElement("div")
+
+    firstLastCharecterDiv.innerHTML = getInitials(ro.clientName);
+    
+    usernameDiv.innerHTML = ro.clientName
+    messageDiv.innerHTML = ro.chatboxMessage
+
+    
+    // firstLastCharecterDiv.setAttribute("class", "golla")
+    firstLastCharecterDiv.setAttribute("class", "userIntial")
+    usernameDiv.setAttribute("class", "usernameDiv")
+    messageDiv.setAttribute("class", "chatChunk")
+    chatPortionContainer.setAttribute("class", "chatContainer")
+
+
+    chatPortionContainer.appendChild(firstLastCharecterDiv)
+    chatPortionContainer.appendChild(usernameDiv)
+    chatPortionContainer.appendChild(messageDiv)
+
+    chatOutput.appendChild(chatPortionContainer)
+
+}
+
+function getInitials(username) {
+    var firstName = username.split(" ")[0];
+    var lastName = username.split(" ")[1];
+    var intials = firstName.charAt(0).toUpperCase() + lastName.charAt(0).toUpperCase();
+    return intials;
+
+}
