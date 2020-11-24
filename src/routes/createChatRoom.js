70b8e268fa714940f16afa0afda9fa48ad589dca
Galia Rabbani <galia.giti@gmail.com>
2020-11-24 22:20:06 +0600

    Implement a room's name link and password.

--- src/routes/createChatRoom.js
+++ src/routes/createChatRoom.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const randomString = require("randomstring");
+
+const PASSWORD_LENGTH = 16;
 
 router.post("/createChatRoom", (req, res, next) => {
-    console.log(req.body)
-    res.render('chat')
+  const roomPassword = randomString.generate(PASSWORD_LENGTH);
+  const { roomName } = req.body;
 
+  const modifiedRoomName = roomName.trim().replace(/ /g, "_").toLowerCase();
+
+  console.log(req.body, roomPassword);
+
+  res.render("chat", {
+    roomName,
+    modifiedRoomName,
+    roomPassword,
+  });
 });
 
 module.exports = router;
-
-
