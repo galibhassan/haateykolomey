2d21c631400614b2075948111c0a82c2779cf7d5
Galia Rabbani <galia.giti@gmail.com>
2021-02-04 20:29:24 +0600

    Clean up

--- src/routes/createChatRoom.js
+++ src/routes/createChatRoom.js
@@ -4,20 +4,16 @@ const randomString = require("randomstring");
 
 const PASSWORD_LENGTH = 16;
 
-// module.exports = router;
-module.exports = (serversideIO) => {
-  router.post("/createChatRoom", (req, res, next) => {
-    const roomPassword = randomString.generate(PASSWORD_LENGTH);
-    const { roomName } = req.body;
-
-    console.log("create chat room route reached")
-    console.log(serversideIO);
-
-    res.render("chat", {
-      roomName,
-      roomPassword,
-    });
-  });
+router.post("/createChatRoom", (req, res, next) => {
+  const roomPassword = randomString.generate(PASSWORD_LENGTH);
+  const { roomName } = req.body;
   
-  return router
-};
+  console.log("create chat room route reached");
+  
+  res.render("chat", {
+    roomName,
+    roomPassword,
+  });
+});
+
+module.exports = router;
