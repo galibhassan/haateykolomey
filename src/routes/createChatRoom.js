14bc2c814b9fac1789e468100095a134120610ee
Galia Rabbani <galia.giti@gmail.com>
2021-02-07 01:45:35 +0600

    Prevent room creation with same name, write room info into db.

--- src/routes/createChatRoom.js
+++ src/routes/createChatRoom.js
@@ -1,17 +1,54 @@
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const randomString = require("randomstring");
+const executeSQLQuery = require("../db/dbManager");
 
 const PASSWORD_LENGTH = 16;
 
-router.post("/createChatRoom", (req, res, next) => {
-  const serversideIO = req.app.get("socketio");
+router.post("/createChatRoom", async (req, res, next) => {
   const roomPassword = randomString.generate(PASSWORD_LENGTH);
   const { roomName } = req.body;
   roomNameSanitized = roomName.trim().replace(/ /g, "");
 
+  /*   const debResponse =  executeSQLQawaituery(
+    `insert into rooms (room_name, room_password)
+     values('${roomNameSanitized}', '${roomPassword}')
+    `
+  )
+  .catch(err => {
+    console.log(err);
+    res.send(err)
+  })
+  */
 
-  res.redirect(`chatsession/${roomNameSanitized}`);
+  const dbResponse = await executeSQLQuery(
+    `select room_name from rooms
+   where room_name = '${roomNameSanitized}'`
+  ).catch((err) => {
+    console.log(err);
+    res.send(err);
+  });
+
+  console.log(dbResponse.length);
+  if (dbResponse.length > 0) {
+    // there exists a room with the proposed roomName
+    res.send(`
+      <h2> A room named ${roomNameSanitized} already exists.
+      Please try with a different room-name. </h2>
+    `);
+  } else {
+    const dbResponse2 = await executeSQLQuery(
+      `insert into rooms (room_name, room_password)
+       values('${roomNameSanitized}', '${roomPassword}')
+      `
+    ).catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
+
+    res.redirect(`chatsession/${roomNameSanitized}`);
+  }
 });
 
 module.exports = router;
