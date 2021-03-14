3cfacfc97ec81b353de71e7670e787bf3d1e3d3e
Galia Rabbani <galia.giti@gmail.com>
2021-03-14 09:29:04 +0600

    Fix registration bug

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const path = require("path");
 const router = express.Router();
+const executeSQLQuery = require("../db/dbManager");
 
 router.get("/register", (req, res, next) => {
-  //const currentPath = path.resolve(__dirname, '../', "view", "register.html");
-  //res.sendFile(currentPath);
- res.render('register')
+  res.render("register");
 });
 
 router.post("/register", (req, res, next) => {
@@ -14,25 +12,29 @@ router.post("/register", (req, res, next) => {
   if (password !== confirmPassword) {
     res.send("Password mismatch");
   } else {
-    amaderDb.all(`select * from users where email = '${userEmail}'`, (err, dbResponse) => {
-      if (dbResponse.length !== 0) {
-        // email already exists
-        res.send("email already exists");
-      } else {
-        // no email address. So you can register
-        const query = `
+    executeSQLQuery(`select * from users where email = '${userEmail}'`)
+      .then((dbResponse) => {
+        if (dbResponse.length !== 0) {
+          // email already exists
+          res.send("email already exists");
+        } else {
+          // no email address. So you can register
+          const query = `
               insert into users (username, firstname, lastname, password, confirm_password, email)
               values ('${username.trim()}', '${firstname.trim()}', '${lastname.trim()}', '${password}', '${confirmPassword}', '${userEmail.trim()}') 
           `;
-        amaderDb.run(query, (err) => {
-          if (err) {
-            res.send(err);
-          } else {
-            res.send("registration success! please login!");
-          }
-        });
-      }
-    });
+          executeSQLQuery(query)
+            .then(() => {
+              res.send("registration successful! please login!");
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 });
 
